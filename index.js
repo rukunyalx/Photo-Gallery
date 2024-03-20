9f9444ed0a2c214ab3a39c4b4b0d6cdd15fba4bf
aleque <rukunyalx@gmail.com>
2024-03-20 11:46:21 +0300

    fixed the fetched data to show on the front end

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btnEl = document.getElementById("btn");
 const errorMessageEl = document.getElementById("errorMessage");
+const galleryEl = document.getElementById("gallery");
 
 
 
@@ -11,17 +12,31 @@ async function fetchImage (){
         errorMessageEl.innerText = "Number be between 0 - 11";
         return;
     }
+    imgs = " ";
+
 
     try {
+      btnEl.style.display = "none";
       const response = await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random() * 1000)}&client_id=8YzVxy9oq5w8iacwsgP_3mB0oWItgbn_N7wTSz916yM`);
       const data = await response.json();
-      console.log(data)
+        if(data){
+          data.forEach((pic) => {
+            imgs +=`
+            <img src=${pic.urls.small} alt="image"/>`;
+            galleryEl.style.display = "block"
+            galleryEl.innerHTML = imgs;
+            btnEl.style.display = "block";
+          
+
+          });
+        }
         
 
         errorMessageEl.style.display = "none";
         } catch (error) {
             errorMessageEl.style.display = "block";
             errorMessageEl.innerHTML = "An error happened try again later";
+            btnEl.style.display = "none";
         }
 }
 
