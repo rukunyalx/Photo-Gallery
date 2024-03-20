c5dad526741f0ef4bf7d0352c8809e6ab4b5703e
aleque <rukunyalx@gmail.com>
2024-03-20 13:16:55 +0300

    Added a spinner

--- index.js
+++ index.js
@@ -12,17 +12,22 @@ async function fetchImage (){
         errorMessageEl.innerText = "Number be between 0 - 11";
         return;
     }
+
     imgs = " ";
 
 
     try {
       btnEl.style.display = "none";
+      const loading = `<img src ="spinner.svg" />`;
+      galleryEl.innerHTML - loading;
+
       const response = await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random() * 1000)}&client_id=8YzVxy9oq5w8iacwsgP_3mB0oWItgbn_N7wTSz916yM`);
       const data = await response.json();
         if(data){
           data.forEach((pic) => {
             imgs +=`
             <img src=${pic.urls.small} alt="image"/>`;
+            
             galleryEl.style.display = "block"
             galleryEl.innerHTML = imgs;
             btnEl.style.display = "block";
